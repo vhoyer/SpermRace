912f138bc039a7b9de38deaf6a895b660077e397
vhoyer <vhoyer@live.com>
2016-12-20 00:12:58 -0200

    Initial Commit
    Signed-off-by: vhoyer <vhoyer@live.com>

---
+++ src/SpermRace.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SpermRace extends Core implements KeyListener,MouseMotionListener,MouseListener,MouseWheelListener {
+	public void setup(){
+		setDefaultListeners();
+	}
+	public synchronized void draw(Graphics2D g){
+	}
+}
